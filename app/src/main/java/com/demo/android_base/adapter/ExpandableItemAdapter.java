d0798c77dfc3e4b37492efe828849acd8e388119
tea9 <shaomiaojava@gmail.com>
2018-11-21 09:36:45 +0800

    add

---
+++ app/src/main/java/com/demo/android_base/adapter/ExpandableItemAdapter.java
@@ -0,0 +1,7 @@
+package com.demo.android_base.adapter;
+
+/**
+ * created by tea9 at 2018/11/18
+ */
+//public class ExpandableItemAdapter extends BaseMultiItemQuickAdapter<MultiItemEntity, BaseViewHolder> {
+//}
